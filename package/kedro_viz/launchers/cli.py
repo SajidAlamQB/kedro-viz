81d33a939aa5d0e1ecca94bbfa72696f4ad3efa2
Nok Chan <nok.lam.chan@quantumblack.com>
2023-10-13 16:00:41 +0100

    Fix the CLI
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- package/kedro_viz/launchers/cli.py
+++ package/kedro_viz/launchers/cli.py
@@ -1,21 +1,29 @@
 """`kedro_viz.launchers.cli` launches the viz server as a CLI app."""
-
 import json
 import logging
 import multiprocessing
+import tempfile
 import traceback
 from datetime import datetime
 from pathlib import Path
 from typing import Dict
 
 import click
+import fsspec
+from jinja2 import Environment, FileSystemLoader
 from kedro.framework.cli.project import PARAMS_ARG_HELP
 from kedro.framework.cli.utils import KedroCliError, _split_params
 from kedro_viz import __version__
 from kedro_viz.api.rest.responses import save_api_responses_to_fs
-from kedro_viz.constants import _HTML_DIR, _METADATA_PATH, DEFAULT_HOST, DEFAULT_PORT
-from kedro_viz.integrations.pypi import get_latest_version, is_running_outdated_version
+from kedro_viz.constants import (_HTML_DIR, _METADATA_PATH, DEFAULT_HOST,
+                                 DEFAULT_PORT)
+from kedro_viz.data_access import DataAccessManager, data_access_manager
+from kedro_viz.integrations.kedro import data_loader as kedro_data_loader
+from kedro_viz.integrations.kedro import telemetry as kedro_telemetry
+from kedro_viz.integrations.pypi import (get_latest_version,
+                                         is_running_outdated_version)
 from kedro_viz.launchers.utils import _check_viz_up, _start_browser, _wait_for
+from kedro_viz.server import populate_data
 from semver import VersionInfo
 from watchgod import RegExpWatcher, run_process
 
@@ -217,40 +225,85 @@ def viz_build(api_dir, host, port, pipeline, env, params):
                 fg="yellow",
             ),
         )
-
-    def _copy_static_files(html_dir: Path, destination_dir):
-        """Upload static HTML files to S3."""
-        logger.debug("Uploading static html files to %s.", self._bucket_path)
-        try:
-            self._remote_fs.put(f"{str(html_dir)}/*", self._bucket_path, recursive=True)
-            self._ingest_heap_analytics()
-        except Exception as exc:  # pragma: no cover
-            logger.exception("Upload failed: %s ", exc)
-            raise exc
-
-    def _upload_deploy_viz_metadata_file(destination_dir):
-        logger.debug(
-            "Creating and Uploading deploy viz metadata file to %s.",
-            destination_dir,
-        )
-
-        try:
-            metadata = {
-                "timestamp": datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S"),
-                "version": str(VersionInfo.parse(__version__)),
-            }
-            with self._remote_fs.open(
-                f"{self._bucket_path}/{_METADATA_PATH}", "w"
-            ) as metadata_file:
-                metadata_file.write(json.dumps(metadata))
-        except Exception as exc:  # pragma: no cover
-            logger.exception("Upload failed: %s ", exc)
-            raise exc
-
     def _deploy(api_dir):
+        _fs = fsspec.filesystem("file")
+
+        def _copy_static_files(html_dir: Path, destination_dir):
+            """Upload static HTML files to S3."""
+            logger.debug("Uploading static html files to %s.", destination_dir)
+
+
+        def _ingest_heap_analytics(destination_dir):
+            """Ingest heap analytics to index file in the build folder."""
+            project_path = Path.cwd().absolute()
+            heap_app_id = kedro_telemetry.get_heap_app_id(project_path)
+            heap_user_identity = kedro_telemetry.get_heap_identity()
+            should_add_telemetry = bool(heap_app_id) and bool(heap_user_identity)
+            html_content = (_HTML_DIR / "index.html").read_text(encoding="utf-8")
+            injected_head_content = []
+
+            env = Environment(loader=FileSystemLoader(_HTML_DIR))
+
+            if should_add_telemetry:
+                logger.debug("Ingesting heap analytics.")
+                telemetry_content = env.get_template("telemetry.html").render(
+                    heap_app_id=heap_app_id, heap_user_identity=heap_user_identity
+                )
+                injected_head_content.append(telemetry_content)
+
+            injected_head_content.append("</head>")
+            html_content = html_content.replace("</head>", "\n".join(injected_head_content))
+
+            with tempfile.TemporaryDirectory() as temp_dir:
+                temp_file_path = f"{temp_dir}/index.html"
+
+                with open(temp_file_path, "w", encoding="utf-8") as temp_index_file:
+                    temp_index_file.write(html_content)
+
+                _fs.put(temp_file_path, f"{destination_dir}/")
+
+        def _upload_deploy_viz_metadata_file(destination_dir):
+            logger.debug(
+                "Creating and Uploading deploy viz metadata file to %s.",
+                destination_dir,
+            )
+
+            try:
+                metadata = {
+                    "timestamp": datetime.utcnow().strftime("%d.%m.%Y %H:%M:%S"),
+                    "version": str(VersionInfo.parse(__version__)),
+                }
+                with _fs.open(
+                    f"{destination_dir}/{_METADATA_PATH}", "w"
+                ) as metadata_file:
+                    metadata_file.write(json.dumps(metadata))
+            except Exception as exc:  # pragma: no cover
+                logger.exception("Upload failed: %s ", exc)
+                raise exc
+
+            try:
+                _fs.put(f"{str(_HTML_DIR)}/*", destination_dir, recursive=True)
+                _ingest_heap_analytics(destination_dir)
+            except Exception as exc:  # pragma: no cover
+                logger.exception("Upload failed: %s ", exc)
+                raise exc
+
         save_api_responses_to_fs(api_dir)
         _copy_static_files(_HTML_DIR, api_dir)
         _upload_deploy_viz_metadata_file(api_dir)
 
+    path = Path.cwd()
+
+    catalog, pipelines, session_store, stats_dict = kedro_data_loader.load_data(
+        path, env,params
+    )
+    pipelines = (
+        pipelines
+        if pipeline is None
+        else {pipeline: pipelines[pipeline]}
+    )
+    populate_data(
+        data_access_manager, catalog, pipelines, session_store, stats_dict
+    )
     _deploy(api_dir)
     print("Kedro Viz Static Site Build successfully...")
